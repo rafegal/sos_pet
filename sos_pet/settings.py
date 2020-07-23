75d978fb9e0dd1a488ca428cb7360ef133361c54
rfgalleani <rfgalleani>
2020-07-23 14:14:12 +0100

    fix invalid import in settings

--- sos_pet/settings.py
+++ sos_pet/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from sos_pet.local_settings import DATABASES
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
